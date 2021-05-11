d6f87d1bb13cae7696cde9cb89bbdcd8cc108f85
Pooya Behravesh <pooyabehravesh@gmail.com>
2021-05-11 21:14:23 +0430

    General infrastructure changes

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import Index from "./screens/index";
+import FirstLoading from "./screens/LoadingPage/FirstLoading";
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async loadFonts() {
+    try {
+      SplashScreen.preventAutoHideAsync();
+      await Font.loadAsync({
+        Kalameh: {
+          uri: require("./assets/font/Kalameh.ttf"),
+        },
+      });
+    } catch {
+      console.log("EveryThing Fucked!");
+    } finally {
+      this.setState({ fontsLoaded: true });
+      SplashScreen.hideAsync();
+    }
+  }
+  UNSAFE_componentWillMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    return <Index />;
+  }
+}
