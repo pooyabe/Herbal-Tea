1140ea732980b3b2d3b529b0787b36826cbf1813
Pooya Behravesh <pooyabehravesh@gmail.com>
2021-05-11 13:39:58 +0430

    Installed supabase ext

--- screens/TestPage/test.js
+++ screens/TestPage/test.js
@@ -2,6 +2,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
+
+
+
 const logOut = async () => {
     await AsyncStorage.setItem("@logged_in", '0');
     setSetLogOutState(1);
