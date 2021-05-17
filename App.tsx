95b7a4b18dc455542315a9f8a79eefca324a0d96
Pooya Behravesh <pooyabehravesh@gmail.com>
2021-05-17 23:51:40 +0430

    Font problem solved

--- App.tsx
+++ App.tsx
@@ -31,7 +31,7 @@ export default class App extends React.Component {
       SplashScreen.hideAsync();
     }
   }
-  UNSAFE_componentWillMount() {
+  componentDidMount() {
     this.loadFonts();
   }
 
