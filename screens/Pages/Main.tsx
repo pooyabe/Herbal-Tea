901afe4deaa6122543cfad8bf8e7da5e145a78d3
Pooya Behravesh <pooyabehravesh@gmail.com>
2021-06-03 19:50:54 +0430

    Added icons to tab navigation

--- screens/Pages/Main.tsx
+++ screens/Pages/Main.tsx
@@ -1,10 +1,10 @@
-/* 
-    TODO: Apply some style to TABS
-*/
-
 import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Sizes from "./Sizes";
 import Chart from "./Chart";
@@ -15,7 +15,37 @@ const Tab = createBottomTabNavigator();
 export default function Index() {
   return (
     <>
-      <Tab.Navigator initialRouteName="Sizes" backBehavior="initialRoute">
+      <Tab.Navigator
+        initialRouteName="Sizes"
+        backBehavior="initialRoute"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Chart") {
+              iconName = focused ? "ios-pie-chart" : "ios-pie-chart-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+            } else if (route.name === "Sizes") {
+              iconName = "pagelines";
+              return <FontAwesome name={iconName} size={size} color={color} />;
+            } else if (route.name === "SendPic") {
+              iconName = "image-filter-tilt-shift";
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+          showLabel: !1
+        }}
+      >
         <Tab.Screen name="Chart" component={Chart} />
         <Tab.Screen name="Sizes" component={Sizes} />
         <Tab.Screen name="SendPic" component={SendPic} />
